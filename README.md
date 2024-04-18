ea6b462293a0827d4b7c86174669aa1b17a2109e
Alina Zueva <alina.milka1466@yandex.ru>
2024-04-18 22:00:33 +0300

    develop_commit_My_first_commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# qa_python
+# qa_python - Sprint_4
+
+2 теста для метода add_new_book:
+
+1 тест: test_add_new_book_add_one_book_with_different_lenght_name - параметризованный тест. Проверяю добавление в словарь books_genre книг с разной длиной наименования: в 1 символ, в 19 символов и в 40 символов - в пределах разрешенного диапазона длины наименования
+
+2 тест: test_add_new_book_not_to_add_book_with_too_long_name_without_name - параметризованный тест. Проверяем, что добавить в словарь books_genre книг длина названия которых: 0 символов и более 40 символов - за пределами разрешенного диапазона длины наименования
+
+Метод set_book_genre:
+ - test_set_book_genre_add_genre_from_list_genre - проверяем добавление жанра для книги, который есть в списке жанров genre.
+
+Метод get_book_genre:
+- test_get_book_genre_get_right_genre_of_book_by_name_book - параметризованный тест. Проверяем соответствие выводимого жанра книги по её имени
+
+Метод get_books_with_specific_genre:
+- test_get_books_with_specific_genre_get_list_of_books_with_one_genre - проверяем правильность вывода списка книг имеющих один и тот же жанр
+
+Метод get_books_genre:
+- test_get_books_genre_return_current_books_genre - проверяем правильный вывод из словаря books_genre, который получаем методом get_books_genre содержание словаря books_genre.
+
+Метод get_books_for_children:
+- test_get_books_for_children_get_books_without_rating - проверяем, что метод не выводит в список книг для детей, которые им подходят. У жанра книги не должно быть возрастного рейтинга, который присутствует в списке genre_age_rating.
+
+Метод add_book_in_favorites:
+- test_add_book_in_favorites_add_book_from_books_genre - проверяем добавление книги, находящейся в словаре books_genre, в список favorites
+
+Метод delete_book_from_favorites:
+- test_delete_book_from_favorites_delete_one_book - проверяем удаляются ли книги, которые были добавлены в список favorites
+
+Метод get_list_of_favorites_books:
+- test_get_list_of_favorites_books_get_whole_list_books_from_favorites - проверяем соответствие списка, получаемого методом get_list_of_favorites_books в список favorites
--- tests.py
+++ tests.py
@@ -1,24 +1,114 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.mark.parametrixe('name_book', ['А', 'Алиса в стране чуда', 'Невероятная грустная история о Эренндире']
+    def test_add_new_book_add_two_books_with_different_lenght_name(self, name_book):
         collector = BooksCollector()
+        collector.add_new_book(name_book)
+        expected_result = {name_book: ''}
+        actual_result == collector.get_books_genre()
+        assert actual_result == expected_result
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    @pytest.mark.parametrize('name_book', ['', 'Удивительное путешествие Нильса Хольгерсона'])
+    def test_add_new_book_not_to_add_book_with_too_long_name_without_name(self, name_book):
+        collector = BooksCollector()
+        collector.add_new_book(name_book)
+        expected_result = {}
+        actual_result = collector.get_books_genre()
+        assert actual_result == expected_result
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_add_genre_from_list_genre(self):
+        collector = BooksCollector()
+        name_book = 'Семь ликов страха'
+        genre_book = 'Ужасы'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre_book)
+        result = collector.get_book_genre(name_book)
+        assert result == genre_book
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize('name_book, genre_book', [['Фауст', 'Трагедия'],
+                                                       ['Алиса в стране чуда', 'Сказка'],
+                                                       ['Четвёртое крыло', 'Фэнтези'],
+                                                       ['Я знаю, что видел', 'Детектив'],
+                                                       ['Семь ликов страха', 'Ужасы']])
+    def test_get_book_genre_get_right_genre_of_book_by_name_book(self, name_book, genre_book):
+        collector = BooksCollector()
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre_book)
+        result = collector.get_book_genre(name_book)
+        assert result == genre_book
+
+    def test_get_books_with_specific_genre_get_list_of_books_with_one_genre(self):
+        collector = BooksCollector()
+        name_book_1 = 'Семь ликов страха'
+        name_book_2 = 'Шкатулка'
+        name_book_3 = 'Я знаю, что видел'
+        genre_book_horror = 'Ужасы'
+        genre_book_detective = 'Детектив'
+        collector.add_new_book(name_book_1)
+        collector.set_book_genre(name_book_1, genre_book_horror)
+        collector.add_new_book(name_book_2)
+        collector.set_book_genre(name_book_2, genre_book_horror)
+        collector.add_new_book(name_book_3)
+        collector.set_book_genre(name_book_3, genre_book_detective)
+        result = collector.get_books_with_specific_genre(genre_book_horror)
+        assert result == [name_book_1, name_book_2]
+
+    def test_get_books_genre_return_current_books_genre(self):
+        collector = BooksCollector()
+        name_book = 'Семь ликов страха'
+        genre_book = 'Ужасы'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre_book)
+        expected_result = {name_book: genre_book}
+        actual_result = collector.get_books_genre()
+        assert actual_result == expected_result
+
+    def test_get_books_for_children_get_books_without_rating(self):
+        collector = BooksCollector()
+        name_book_1 = 'Семь ликов страха'
+        name_book_2 = 'Я знаю, что видел'
+        genre_book_horror = 'Ужасы'
+        genre_book_comedy = 'Детектив'
+        collector.add_new_book(name_book_1)
+        collector.set_book_genre(name_book_1, genre_book_horror)
+        collector.add_new_book(name_book_2)
+        collector.set_book_genre(name_book_2, genre_book_detective)
+        expected_result = [name_book_2]
+        actual_result = collector.get_books_for_children()
+        assert actual_result == expected_result
+
+    def test_add_book_in_favorites_add_book_from_books_genre(self):
+        collector = BooksCollector()
+        name_book = 'Семь ликов страха'
+        collector.add_new_book(name_book)
+        collector.add_book_in_favorites(name_book)
+        expected_result = [name_book]
+        actual_result = collector.get_list_of_favorites_books()
+        assert actual_result == expected_result and len(actual_result) == 1
+
+    def test_delete_book_from_favorites_delete_one_book(self):
+        collector = BooksCollector()
+        name_book = 'Семь ликов страха'
+        collector.add_new_book(name_book)
+        collector.add_book_in_favorites(name_book)
+        collector.delete_book_from_favorites(name_book)
+        expected_result = []
+        actual_result = collector.get_list_of_favorites_books()
+        assert actual_result == expected_result and len(actual_result) == 0
+
+    def test_get_list_of_favorites_books_get_whole_list_books_from_favorites(self):
+        collector = BooksCollector()
+        name_book_1 = 'Семь ликов страха'
+        name_book_2 = 'Я знаю, что видел'
+        collector.add_new_book(name_book_1)
+        collector.add_new_book(name_book_2)
+        collector.add_book_in_favorites(name_book_1)
+        collector.add_book_in_favorites(name_book_2)
+        expected_result = [name_book_1, name_book_2]
+        actual_result = collector.get_list_of_favorites_books()
+        assert actual_result == expected_result and len(actual_result) == 2
+
+
